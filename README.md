a705c1a01129225c240496db5eaa10d219739e0a
Zach Rausnitz <zjraus@gmail.com>
2016-10-16 15:52:47 -0400

    Add NYT forecast

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ This bot scrapes election forecasts and [tweets](https://twitter.com/horseracebo
 The bot tweets when...
 
 Any of the following happens on FiveThirtyEight's 2016 presidential election [forecast](http://projects.fivethirtyeight.com/2016-election-forecast/):
-- Hillary Clinton's chance of winning (rounded to the nearest whole number) changes.
-- Donald Trump's chance of winning (rounded to the nearest whole number) changes.
+- Hillary Clinton's chance of winning changes.
+- Donald Trump's chance of winning changes.
 - The list of the five most competitive states changes.
 
-(additional forecasts TK)
+Any of the following happens on The New York Times' 2016 presidential election [forecast](http://www.nytimes.com/interactive/2016/upshot/presidential-polls-forecast.html):
+- Hillary Clinton's chance of winning changes.
+- Donald Trump's chance of winning changes.
 
 ## License
 
--- app.js
+++ app.js
@@ -19,7 +19,34 @@ function send_tweet(message) {
   }
 }
 
+function update_record(target_file, scrape) {
+  fs.readFile(target_file, function(err, data) {
+    if (err && err.code == 'ENOENT') {
+      fs.writeFile(target_file, JSON.stringify({'current': scrape, 'previous': {}}));
+    } else if (!err) {
+      var record = JSON.parse(data);
+      record.previous = record.current;
+      record.current = scrape;
+      fs.writeFile(target_file, JSON.stringify(record), function(err) {
+        if (err) throw err;
+        if (!_.isEqual(record.current, record.previous)) {
+          if (target_file == 'record_538.json') {
+            var message = 'New @FiveThirtyEight forecast. Chance of winning: Clinton ' + record.current.clinton_chance + '%, Trump ' + record.current.trump_chance + '%. Tipping points: ' + record.current.close_states.join(', ') + '. http://projects.fivethirtyeight.com/2016-election-forecast/';
+          } else if (target_file == 'record_nyt.json') {
+            var message = 'New @UpshotNYT forecast. Chance of winning: Clinton ' + record.current.clinton_chance + ', Trump ' + record.current.trump_chance + '. http://www.nytimes.com/interactive/2016/upshot/presidential-polls-forecast.html';
+          }
+          console.log('New: ' + JSON.stringify(scrape));
+          send_tweet(message);
+        } else {
+          console.log('Unchanged: ' + JSON.stringify(scrape));
+        }
+      });
+    }
+  });
+}
+
 var browser = new phantomJsCloud.BrowserApi(process.env.PHANTOMJSCLOUD_KEY);
+
 browser.requestSingle({ url: 'http://projects.fivethirtyeight.com/2016-election-forecast/', renderType: 'html', ignoreImages: true}, function(err, userResponse) {
   if (err) throw err;
   var $ = cheerio.load(userResponse.content.data);
@@ -46,23 +73,17 @@ browser.requestSingle({ url: 'http://projects.fivethirtyeight.com/2016-election-
     'trump_chance': trump_chance,
     'close_states': close_states
   };
-  fs.readFile('record.json', function(err, data) {
-    if (err && err.code == 'ENOENT') {
-      fs.writeFile('record.json', JSON.stringify({'current': scrape, 'previous': {}}));
-    } else if (!err) {
-      var record = JSON.parse(data);
-      record.previous = record.current;
-      record.current = scrape;
-      fs.writeFile('record.json', JSON.stringify(record), function(err) {
-        if (err) throw err;
-        if (!_.isEqual(record.current, record.previous)) {
-          var message = 'New @FiveThirtyEight forecast. Chance of winning: Clinton ' + record.current.clinton_chance + '%, Trump ' + record.current.trump_chance + '%. Tipping points: ' + record.current.close_states.join(', ') + '. http://projects.fivethirtyeight.com/2016-election-forecast/';
-          console.log('New: ' + JSON.stringify(scrape));
-          send_tweet(message);
-        } else {
-          console.log('Unchanged: ' + JSON.stringify(scrape));
-        }
-      });
-    }
-  })
+  update_record('record_538.json', scrape);
+});
+
+browser.requestSingle({ url: 'http://www.nytimes.com/interactive/2016/upshot/presidential-polls-forecast.html', renderType: 'html', ignoreImages: true}, function(err, userResponse) {
+  if (err) throw err;
+  var $ = cheerio.load(userResponse.content.data);
+  var clinton_chance = $('.clinton-est').html(),
+      trump_chance = $('.trump-est').html();
+  var scrape = {
+    'clinton_chance': clinton_chance,
+    'trump_chance': trump_chance
+  };
+  update_record('record_nyt.json', scrape);
 });
